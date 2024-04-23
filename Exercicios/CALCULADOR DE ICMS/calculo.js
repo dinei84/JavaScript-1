372e3deb1828cf6964ec19e5acc88569b8f9507e
dinei84 <162598350+dinei1984@users.noreply.github.com>
2024-04-23 17:51:20 -0300

    calculadora de icms inicio

---
+++ Exercicios/CALCULADOR DE ICMS/calculo.js
@@ -0,0 +1,15 @@
+// valordoFrete
+// margem
+// estadoSaida
+// estadoDestino
+
+let ms,mt,go = 7 
+
+const valordoFrete = document.getElementById('valordoFrete')
+const margem = document.getElementById('margem')
+
+const calcular = document.getElementById('calcular')
+const limpar = document.getElementById('limpar')
+
+
+console.log(valordoFrete)
