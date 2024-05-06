3f786292cc5109e653c231604b3865f474af35ae
dinei84 <devdinei@gmail.com>
2024-05-06 17:58:13 -0300

    estudos

--- Exercicios/ARRAY/INTROD A ARRAY, ISERINDO E OUTROS/script.js
+++ Exercicios/ARRAY/INTROD A ARRAY, ISERINDO E OUTROS/script.js
@@ -5,7 +5,7 @@ let cursos = ['Javascript','css','HTML','PHP',cores]
 
 // cursos [0]=`Aqui ficam os cursos de: ` + 2024
 
-// cursos.push('C++','Python','C#')
+cursos.push('C++','Python','C#')
 // cursos.unshift('Machine lerning') // Adiciona um elemento no inicio do array 
 // cursos.shift() // retura o primeiro elemento do array
 // cursos.pop() // retira o ultimo elemento do array
@@ -14,7 +14,7 @@ let cursos = ['Javascript','css','HTML','PHP',cores]
 console.log(cursos[4][4][2])
 
 cursos.map((el)=>{
-    let p = document.createElement('p')
-    p.innerHTML=el
-    caixa.appendChild(p)
+    let div = document.createElement('div')
+    div.innerHTML=el
+    caixa.appendChild(div)
 })
