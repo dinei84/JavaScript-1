8261724a292186b59d8511534d40fdc21d04b9bf
dinei84 <devdinei@gmail.com>
2024-08-15 09:34:03 -0300

    Exercicios

--- Exercicios/TESTES/MASCARA/script.js
+++ Exercicios/TESTES/MASCARA/script.js
@@ -1,3 +1,10 @@
-const cardNumber = 123458522528
-<
-if(cardNumber.valueOf.length )
+function maskify(cc) {
+    if (cc.length <= 4) {
+        return cc;
+    } else {
+        return '#'.repeat(cc.length - 4) + cc.slice(-4);
+    }
+}
+
+
+console.log(maskify(1524255859555))
