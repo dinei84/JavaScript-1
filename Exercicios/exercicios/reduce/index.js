6156d211cd549d460f43d5634a54ca099d0f35dd
dinei84 <devdinei@gmail.com>
2024-09-27 16:16:04 -0300

    update - index

--- Exercicios/exercicios/reduce/index.js
+++ Exercicios/exercicios/reduce/index.js
@@ -1,10 +1,14 @@
 // Função que conta a quantidade de elementos de um array
 
-const fruits = ['apple', 'banana', 'orange', 'apple', 'orange', 'banana'];
-
-const countFruits = fruits.reduce((acc, fruit) => {
-    acc[fruit] = (acc[fruit] || 0) + 1;
-    return acc;
-}, {});
-
-console.log(countFruits); // Saída: { apple: 2, banana: 2, orange: 2 }
+function countFruits(fruits) {
+    return fruits.reduce((acc, fruit) => {
+      acc[fruit] = (acc[fruit] || 0) + 1;
+      return acc;
+    }, {});
+  }
+  
+  module.exports = countFruits;
+  
+  // Exemplo de uso:
+  // const fruits = ['apple', 'banana', 'orange', 'apple', 'orange', 'banana'];
+  // console.log(countFruits(fruits)); // Saída: { apple: 2, banana: 2, orange: 2 }
