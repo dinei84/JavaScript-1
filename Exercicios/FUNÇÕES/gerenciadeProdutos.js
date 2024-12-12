afe28e9dbaf1eff99a6045175fe386be6a791859
dinei84 <devdinei@gmail.com>
2024-12-12 18:05:43 -0300

    mais exercicios

---
+++ Exercicios/FUNÇÕES/gerenciadeProdutos.js
@@ -0,0 +1,39 @@
+const produtos = [
+    {id: 1, nome: "SmarTV",categoria:"Eletrônicos",peso: 35 }
+];
+
+function adicionarProduto(id, nome, categoria, peso){
+    const novoRegistro = {
+        id: id,
+        nome: nome,
+        categoria: categoria,
+        peso: peso
+    };
+    if(novoRegistro.length < produtos.length){
+        return "Esta  faltando al";
+    }
+
+    produtos.push(novoRegistro)
+}
+
+function bucasrProdutoPorId(id){
+    for(let i = 0; i < produtos.length; i++){
+        if(produtos[i].id === id){
+            return produtos[i];
+        }
+    }
+    return null;
+}
+
+function listarProdutos(categoria){
+    for(let i = 0; i < produtos.length; i++){
+        if(produtos[i].categoria === categoria){
+            console.log(produtos[i]);
+        }
+    }
+}
+
+
+adicionarProduto(2, "Notebook", "Eletrônicos", 2.5);
+
+console.log(produtos);
